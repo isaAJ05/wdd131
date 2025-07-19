8f30ec236f00abd2fc0fc018c1c0bb3f75f4f663
Ari <127914863+isaAJ05@users.noreply.github.com>
2025-07-18 22:45:40 -0500

    temple album improvements

---
+++ .history/scripts/temples_20250718211329.js
@@ -0,0 +1,8 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const h1 = document.querySelector('h1');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
---
+++ .history/scripts/temples_20250718211758.js
@@ -0,0 +1,9 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const h1 = document.querySelector(h1');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+    hamButton.classList.toggle('open');
+    h1.classList.toggle('open');
+});
